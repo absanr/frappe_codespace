0aa63917137d633ec1d8e2b39276c15c1a51fa1d
Roger <88864000+absanr@users.noreply.github.com>
2024-12-19 12:28:11 -0500

    fix: validate MariaDB availability and resolve bench new-site issues
    Implemented pre-checks to validate MariaDB accessibility before executing the bench new-site command, reducing potential failures.
    
    Enhanced error handling during site creation by incorporating retry mechanisms and ensuring correct MariaDB configurations.
    
    Ensured compatibility with the root user for database interactions.

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,7 +13,7 @@ rm -rf /workspaces/frappe_codespace/.git
 # Set permissions for the current script
 SCRIPT_PATH="$(realpath "$0")"
 chmod +x "$SCRIPT_PATH"
-echo "Permisos configurados para: $SCRIPT_PATH"
+echo "Permissions set for: $SCRIPT_PATH"
 
 source /home/frappe/.nvm/nvm.sh
 nvm alias default 18
@@ -38,12 +38,39 @@ bench set-redis-socketio-host redis-socketio:6379
 # Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
 
+# ---------------------------------------------------------------------------------------
+# Check MariaDB availability
+# ---------------------------------------------------------------------------------------
+echo "Checking MariaDB availability..."
+until mysql -hmariadb -uroot -p123 -e "SELECT 1;" >/dev/null 2>&1; do
+  echo "Waiting for MariaDB to be available..."
+  sleep 3
+done
+echo "MariaDB is available."
 
-bench new-site dev.localhost \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
+# ---------------------------------------------------------------------------------------
+# Create site using root
+# ---------------------------------------------------------------------------------------
+create_site_with_root() {
+  echo "Trying to create site with root user..."
+  if ! bench new-site dev.localhost \
+    --mariadb-root-username root \
+    --mariadb-root-password 123 \
+    --admin-password admin \
+    --db-host mariadb \
+    --mariadb-user-host-login-scope=%; then
+    echo "Critical error: Failed to create site using root."
+    exit 1
+  else
+    echo "Site created successfully."
+  fi
+}
 
+create_site_with_root               
+
+# ---------------------------------------------------------------------------------------
+# Additional site configuration
+# ---------------------------------------------------------------------------------------
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
 bench use dev.localhost
